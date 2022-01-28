54571fe45bd15a9289177395e4e84d91c9de0463
Perera, Tharaka <tharaka.perera@pearson.com>
2022-01-28 16:53:57 +0530

    1

--- codedeploy/after_install.sh
+++ codedeploy/after_install.sh
@@ -2,4 +2,9 @@
 
 # This script is executed after the source is copied to the instances
 cd /usr/cddemo/app_code || exit
+
+export NVM_DIR="/root/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 npm install
