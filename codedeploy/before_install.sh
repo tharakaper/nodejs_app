9a2e40296ec840e34ea810d7d0c794c00d8dbe90
Perera, Tharaka <tharaka.perera@pearson.com>
2022-01-28 12:02:55 +0530

    1

--- codedeploy/before_install.sh
+++ codedeploy/before_install.sh
@@ -7,10 +7,8 @@ yum -y update
 #curl --silent --location https://rpm.nodesource.com/setup_4.x | bash -
 #yum -y install nodejs
 
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.1/install.sh | bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-nvm install node
+curl --silent --location https://raw.githubusercontent.com/creationix/nvm/v0.32.1/install.sh | bash
+yum -y install nodejs
 
 npm install -g pm2
 pm2 update
